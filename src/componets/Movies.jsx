5eea1c6942bc9a7cc13686f14d9d37a3b44c0346
August610 <54235530+August610@users.noreply.github.com>
2022-07-16 14:56:49 +0300

    radio

--- src/componets/Movies.jsx
+++ src/componets/Movies.jsx
@@ -4,9 +4,9 @@ function Movies(props) {
     const {movies} = props;
 
     return <div className="movies">
-        {movies.map(movie => (
+        {movies?.length ? movies.map(movie => (
              <Movie key={movie.imdbID} {...movie}/>
-        ))}
+        )) : <h4>Nothing found</h4>}
     </div>
 }
 
